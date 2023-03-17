f2caf544e1b010cd4cd5b75a290687c466abcd11
nyrellcl <nyrell.leonor2000@gmail.com>
2023-03-16 22:27:26 -0700

    style laptop view

--- src/pages/components/layout.tsx
+++ src/pages/components/layout.tsx
@@ -16,9 +16,9 @@ export default function Nav() {
               xmlns="http://www.w3.org/2000/svg"
               className="nav-bar__logo"
             >
-              <g fill="none" fill-rule="evenodd">
+              <g fill="none" fillRule="evenodd">
                 <circle fill="#382314" cx="48" cy="24" r="24" />
-                <circle stroke="#FFF" stroke-width="2" cx="24" cy="24" r="23" />
+                <circle stroke="#FFF" strokeWidth="2" cx="24" cy="24" r="23" />
               </g>
             </svg>
 
